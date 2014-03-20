3a7af9454cfd14efc1c95da9b16b39e98dee7d85
Jeff Risberg <jrisberg@aol.com>
2014-03-20 13:46:57 -0700

    changed project structure to implement DB Setup and improved db population file

--- src/main/resources/DB_Populate_10_Users.sql
+++ src/main/resources/DB_Populate_10_Users.sql
@@ -1,3 +1,3 @@
 -- users
 
-INSERT INTO users (id, first_name, last_name, email, password) values (1, 'Bob', 'Jones', 'bjones@aol.com', '123456');
+INSERT INTO user (id, firstName, lastName, email, password) values (1, 'Bob', 'Jones', 'bjones@aol.com', '123456');
