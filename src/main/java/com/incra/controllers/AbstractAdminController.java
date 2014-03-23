facccc17b4ff8fcfbfdff967de843c94963b499f
Jeff Risberg <jrisberg@aol.com>
2014-03-23 16:03:57 -0700

    updated framework versions of menu management, exception management, etc.

---
+++ src/main/java/com/incra/controllers/AbstractAdminController.java
@@ -0,0 +1,16 @@
+package com.incra.controllers;
+
+import org.springframework.security.access.annotation.Secured;
+
+/**
+ * The <i>AbstractAdminController</i> provides utility functions to all
+ * controllers which are carrying out admin CRUD operations.
+ *
+ * @author Jeffrey Risberg
+ * @since 12/05/11
+ */
+@Secured("ROLE_ADMIN")
+public abstract class AbstractAdminController {
+
+    // to be filled in
+}
---
+++ src/main/java/com/incra/controllers/AdminHomeController.java
@@ -0,0 +1,57 @@
+package com.incra.controllers;
+
+import com.incra.controllers.dto.AdminPanel;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * The <i>AdminHomeController</i> controller generates the home screen for all
+ * admin functions, such as Questions, Goals, Levels, and Users.
+ *
+ * @author Jeffrey Risberg
+ * @since 11/15/11
+ */
+@Controller
+public class AdminHomeController {
+    protected static Logger logger = LoggerFactory.getLogger(AdminHomeController.class);
+
+    public AdminHomeController() {
+    }
+
+    /**
+     * Construct a description of the admin screen, out of the panels, then
+     * render it. The code should be checking for permissions on the specific
+     * panels, but it doesn't yet.
+     */
+    @Secured("ROLE_ADMIN")
+    @RequestMapping(value = "/adminHome/**")
+    public ModelAndView index() {
+
+        List<AdminPanel> adminPanelList = new ArrayList<AdminPanel>();
+        AdminPanel adminPanel;
+
+        adminPanel = new AdminPanel("Vendors", "/vendor");
+        adminPanelList.add(adminPanel);
+
+        adminPanel = new AdminPanel("Charities", "/charity");
+        adminPanelList.add(adminPanel);
+
+        adminPanel = new AdminPanel("Donations", "/donation");
+        adminPanelList.add(adminPanel);
+
+        adminPanel = new AdminPanel("Users", "/user");
+        adminPanelList.add(adminPanel);
+
+        ModelAndView modelAndView = new ModelAndView("adminHome/index");
+        modelAndView.addObject("adminPanelList", adminPanelList);
+
+        return modelAndView;
+    }
+}
