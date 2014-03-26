8be0f72d5ab6b5d168e030c98fa7408c0731966d
Jeff Risberg <jrisberg@aol.com>
2014-03-26 13:17:40 -0700

    first tests with CKeditor

--- src/main/resources/DB_Populate_30_Rubrics.sql
+++ src/main/resources/DB_Populate_30_Rubrics.sql
@@ -2,7 +2,7 @@
 
 INSERT INTO rubric (id, parent_id, seq_num, title, body)
 values (1, null, 1, 'Donations',
- '<p><span style="color: rgb(51, 51, 51); font-family: 'Century Gothic', Quicksand, sans-serif; font-size: 14px; line-height: 20px;">
+ '<p><span style="color: rgb(51, 51, 51); font-family: ''Century Gothic'', Quicksand, sans-serif; font-size: 14px; line-height: 20px;">
    We make it easy to support your favorite causes and donate when, how, and as often as you want to more than 1.8 million charities working throughout the world. Several convenient search tools help you quickly find and learn more about any charity.
    </span></p>');
 INSERT INTO rubric (id, parent_id, seq_num, title, body)
