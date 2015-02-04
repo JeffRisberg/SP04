42ce834501733e323ede49a310a0d50b07734d11
JeffRisberg <jrisberg@aol.com>
2015-02-04 00:25:15 -0800

    took out ehcache reference

--- src/main/resources/DB_Populate_10_Users.sql
+++ src/main/resources/DB_Populate_10_Users.sql
@@ -1,4 +1,4 @@
 -- users
 
 INSERT INTO user (id, firstName, lastName, email, password, date_created, last_updated)
-values (1, 'Bob', 'Jones', 'bjones@aol.com', '123456', now(), now());
+values (1, 'Bob', 'Jones', 'bjones@gmail.com', '123456', now(), now());
